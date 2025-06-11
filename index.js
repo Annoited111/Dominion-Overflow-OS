9364ef90ad447aa1856781fc2e75e2d138eb6e8f
Annoited111 <admin@dominionpropertypros.com>
2025-06-10 23:45:57 -0400

    Improve Real Estate link in Home Page

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ export default function Home() {
       <h1 style={{ color: '#0070f3' }}>👑 Dominion Overflow System</h1>
       <p>Welcome to your live AI-powered empire hub.</p>
       <ul>
-        <li><a href="/real-estate">Real Estate Deals</a></li>
+        <li><a href="/real-estate">Real Estate AI Brain</a></li>
         <li><a href="/vehicle-management">Vehicle Management</a></li>
         <li><a href="/book-publishing">Book Publishing</a></li>
         <li><a href="/designer">Designer Tab</a></li>
