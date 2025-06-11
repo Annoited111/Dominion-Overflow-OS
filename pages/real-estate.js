83e3ddf74ea3905fff5378e1337cb5b48e94fa9a
Annoited111 <admin@dominionpropertypros.com>
2025-06-10 20:52:30 -0400

    Create real-estate.js

---
+++ pages/real-estate.js
@@ -0,0 +1,10 @@
+import RealEstateForm from '../components/RealEstateForm';
+
+export default function RealEstatePage() {
+  return (
+    <div style={{ padding: '40px' }}>
+      <h1>Real Estate Deal Brain 🧠</h1>
+      <RealEstateForm />
+    </div>
+  );
+}
